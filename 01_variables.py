211eadfe311f9f555fe7aa815f950b107bda319a
Roxana María Garrido Sánchez <garrido.sanchez.roxana.maria@alumnat.copernic.cat>
2023-02-01 20:53:20 +0100

    first commit

---
+++ 01_variables.py
@@ -0,0 +1,25 @@
+#Variables
+
+MyVariable = "My string variable"
+
+#Convención de python snake una_variable
+
+my_string_variable = "My string variable"
+my_int_variable = 5
+
+#str transforma a string
+my_int_to_str_var = str(my_int_variable)
+
+#type es para saber el tipo de variable
+print (type(my_int_to_str_var))
+
+#A print se les puede pasar varias variables y crea una cadena 
+#Concatenación
+print("My string variable", my_int_variable)
+
+# len() abrev de length
+print(len(my_string_variable))
+
+#Variables en una sola línea
+var1, var2, var3 = 5, "hola", True;
+print(var1, var2, var3)
---
+++ 02_operadores.py
@@ -0,0 +1,37 @@
+#Operadores aritméticos
+
+print(3+4)
+print(3-4)
+print(3*4)
+print(3/4)
+print(10%2) # módulo resto de división
+print(10//3) # división aproximada devuelve int
+print(2**3) #exponente 2 elevado a 3
+
+print("Hola"+"python")
+#print("Hola"+5) # error
+print("Hola"*5) # funciona, repite la string varias veces
+
+#Operadores comparativos
+
+print(3>4)
+print(3<4)
+print(3>=4)
+print(3<=4)
+print(3==4)
+print(3!=4)
+
+#Fira Code fuente
+
+print("Hola" > "Python") #Comparación de strings por orden alfabético ASCII
+print(len("Hola") < len("a")) # Comparación de lengths
+
+#Operadores lógicos and or not lógica booleana
+print("-----------------------")
+print(False and True) # False
+print(True and True) # True
+print(True or False) # True
+print(True or True) # True
+print(not True) # False
+print(not False) # True
+
